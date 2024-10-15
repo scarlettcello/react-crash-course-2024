f331b374367c94d7698af79d758c42c6ce9a6933
scarlettcello <scarlettcello@gmail.com>
2024-10-14 21:06:09 -0400

    Added CRUD functionalities

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify'
+import Navbar from "../components/Navbar";
+import 'react-toastify/dist/ReactToastify.css';
+
+const MainLayout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default MainLayout
